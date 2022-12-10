39d6f37cea10043332db260f35e0840f1fa3815e
Tianabb <Xtyana993@gmail.com>
2022-12-10 09:01:52 -0800

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,9 +7,9 @@
 
 int main(void)
 {
-printf("size of char: 1 byte(s)");
-printf("size of an int: 4 byte(s)");
-printf("size of a long int: 8 byte(s)");
-printf("size of a float: 4 byte(s)");
+printf("Size of char: 1 byte(s)");
+printf("Size of an int: 4 byte(s)");
+printf("Size of a long int: 8 byte(s)");
+printf("Size of a float: 4 byte(s)");
 return (0);
 }
