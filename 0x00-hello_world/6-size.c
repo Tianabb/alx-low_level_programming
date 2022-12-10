dfc149c944aad5bf5793d582c91b3023683e00d8
Tianabb <Xtyana993@gmail.com>
2022-12-10 08:28:40 -0800

    Task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * Main - Entry
+ * Return: 0
+ */
+
+int main(void)
+{
+printf("size of char, size of char");
+printf("size of int, size of int");
+printf("size of float, size of float");
+printf("size of a long int, size of long int");
+printf("size of a long long int, size of long long int");
+return (0);
+}
