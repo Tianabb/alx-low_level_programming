65b220c66d3afe10b2d783c9db5b60627b1c8fc6
Tianabb <Xtyana993@gmail.com>
2022-12-11 09:23:18 -0800

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - print single digit numbers
+ * Return: 0
+ */
+
+int main(void)
+{
+int w;
+for (w = '0'; w <= '9'; w++)
+{
+putchar(w);
+if (w != '9')
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
