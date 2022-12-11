8b825b656ce129e5d064ef02b71ad77e8a9f1a37
Tianabb <Xtyana993@gmail.com>
2022-12-11 08:44:34 -0800

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - 6 print numbers
+ * Return: 0
+ */
+int main(void)
+{
+int w;
+for (w = '0'; w <= '9'; w++)
+{
+putchar(w);
+}
+putchar('\n');
+return (0);
+}
