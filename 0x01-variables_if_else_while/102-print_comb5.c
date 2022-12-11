e2a84b91699a8e6aa847c95394f36250a050e269
Tianabb <Xtyana993@gmail.com>
2022-12-11 10:57:13 -0800

    Task 12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - print combination of two two digit numbers
+ * Return: 0
+ */
+
+int main(void)
+{
+int a, b;
+for (a = 0; a <= 98; a++)
+{
+for (b = a + 1; b <= 99; b++)
+{
+putchar((a / 10) + '0');
+putchar((a % 10) + '0');
+putchar(' ');
+putchar((b / 10) + '0');
+putchar((b % 10) + '0');
+if (a == 98 && b == 99)
+continue;
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
