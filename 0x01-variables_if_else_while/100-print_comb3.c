8fd0835d833d56ff269f1625ae1b9afad90f1594
Tianabb <Xtyana993@gmail.com>
2022-12-11 09:43:02 -0800

    Task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - print combination of two digits
+ * Return: 0
+ */
+
+int main(void)
+{
+int w, n;
+for (w = 0; w < 9; w++)
+{
+for (n = w + 1; n < 10; n++)
+{
+putchar((w % 10) + '0');
+putchar((n % 10) + '0');
+if (w == 8 && n == 9)
+continue;
+putchar(',');
+putchar(' ');
+}
+}
+return (0);
+}
