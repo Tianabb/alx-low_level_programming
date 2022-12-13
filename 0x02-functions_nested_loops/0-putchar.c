910e4d751f48ab74f1b23c94a2c924dbf9766798
Tianabb <Xtyana993@gmail.com>
2022-12-13 10:09:27 -0800

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,12 +8,14 @@
 int main(void)
 
 {
-char word[8] = "_putchar";
-int w;
-
-for (w = 0; w < 8; w++)
-_putchar(word[8]);
-
-_putchar('\n');
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
 return (0);
 }
