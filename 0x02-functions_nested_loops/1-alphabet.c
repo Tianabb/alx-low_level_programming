754db790c2ec733a178d998cbcd984fed80e2198
Tianabb <Xtyana993@gmail.com>
2022-12-13 11:56:50 -0800

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_alphabets - print all alphabets in lowercase
+ */
+void print_alphabets(void)
+
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+_putchar(ch);
+_putchar('\n');
+}
