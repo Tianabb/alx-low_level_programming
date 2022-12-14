9626fa889f741c631828c7efad5c13dd567d312e
Tianabb <117751167+Tianabb@users.noreply.github.com>
2022-12-14 07:55:31 -0800

    Update 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_sign - function that prints the sign of a number
+ * @n: An input character
+ * Description: print the sign of a number
+ * Return: Always 1
+ */
+int print_sign(int n)
+{
+	int value;
+
+	if (n > 0)
+	{
+		value = 1;
+		_putchar('+');
+	}
+	else if (n == 0)
+	{
+		value = 0;
+		_putchar('0');
+	}
+	else
+	{
+		value = -1;
+		_putchar('-');
+	}
+	return (value);
+}
