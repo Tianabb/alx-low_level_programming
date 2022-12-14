399b59fad1115e3434b52fe96629d96d2f9882ec
Tianabb <Xtyana993@gmail.com>
2022-12-14 13:59:45 -0800

    done

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 #include "main.h"
 
 /**
@@ -30,34 +30,4 @@ _putchar('-');
 return (value);
 }
 
-=======
-#include "main.h"
 
-/**
- * print_sign - function that prints the sign of a number
- * @n: An input character
- * Description: print the sign of a number
- * Return: Always 1
- */
-int print_sign(int n)
-{
-	int value;
-
-	if (n > 0)
-	{
-		value = 1;
-		_putchar('+');
-	}
-	else if (n == 0)
-	{
-		value = 0;
-		_putchar('0');
-	}
-	else
-	{
-		value = -1;
-		_putchar('-');
-	}
-	return (value);
-}
->>>>>>> f76309f5a5877dbc571b3694606842fdda8d82ba
