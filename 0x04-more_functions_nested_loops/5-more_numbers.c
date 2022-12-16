30a2b3650ae5fa71c4be43aac9333b3fd6dacd44
Tianabb <Xtyana993@gmail.com>
2022-12-16 13:48:06 -0800

    Task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - number that prints 10 times the number
+ * Return: Always 0
+ */
+void more_numbers(void)
+{
+int w, y = 0;
+
+for (; y < 10; y++)
+{
+for (w = 0; w <= 14; w++)
+{
+if (w > 9)
+_putchar(w / 10 + '0');
+_putchar(w % 10 + '0');
+}
+_putchar('\n');
+}
+}
