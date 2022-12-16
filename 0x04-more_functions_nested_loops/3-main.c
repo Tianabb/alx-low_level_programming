4b9691b6f9cf390aea8bc522538873776e8614aa
Tianabb <Xtyana993@gmail.com>
2022-12-16 13:34:35 -0800

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - check code
+ *
+ * Return: 0
+ */
+int main(void)
+{
+print_numbers();
+return (0);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_numbers - function that prints numbers from 0 to 9
+ * Return: Always 0
+ */
+void print_numbers(void)
+{
+int w = '0';
+
+for (; w <= '9'; w++)
+_putchar(w);
+_putchar('\n');
+}
