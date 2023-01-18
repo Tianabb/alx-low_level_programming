4d7fa12bd012cdcf8951d18a7efd25f3e4185887
Tianabb <Xtyana993@gmail.com>
2023-01-18 12:33:18 -0800

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,15 @@
-/**
- * print_name - function to print name
- *
- * @name: name to print
- * @f: function pointer that doesn't return anything
- *
- * Return: Always 0 (Success)
- */
+#include <stdlib.h>
+#include "function_pointers.h"
 
-void print_name(char *name, void (*f)(char *))
+/**
+ * print_name - a function that prints a name.
+ * @name: pointer to name to print
+ * @f: function to print name
+ * Return: Nothing
+ */
+void print_name(char *name, void (*f)(char *name))
 {
-if (name && f)
+if (name == NULL || f == NULL)
+return;
 f(name);
 }
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,19 +1,9 @@
-#ifndef FUNCTION_POINTERS_H
-#define FUNCTION_POINTERS_H
+#ifndef MAIN_H
+#define MAIN_H
 
+int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 
-/**
- * enum bool - typedef function for bool
- * @TRUE: 1
- * @FALSE: 0
- */
-typedef enum bool
-{
-TRUE = 1,
-FALSE = 0
-bool;
-}
-#endif /* FUNCTION_POINTERS_H */
+#endif
