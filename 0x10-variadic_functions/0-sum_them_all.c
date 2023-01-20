d64342c4a1e043d2004ecf3fbdda4e52d413f530
Tianabb <Xtyana993@gmail.com>
2023-01-19 16:17:52 -0800

    Task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,3 +1,4 @@
+
 #include "variadic_functions.h"
 #include <stdarg.h>
 #include <stdio.h>
@@ -10,15 +11,15 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-va_list ap;
-unsigned int i = 0;
-int sum = 0;
+	va_list ap;
+	unsigned int i = 0;
+	int sum = 0;
 
-if (n == 0)
-return (0);
-va_start(ap, n);
-for (; i < n; i++)
-sum += va_arg(ap, int);
-va_end(ap);
-return (sum);
+	if (n == 0)
+		return (0);
+	va_start(ap, n);
+	for (; i < n; i++)
+		sum += va_arg(ap, int);
+	va_end(ap);
+	return (sum);
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,12 +1,13 @@
-#ifndef _VARIADIC_FUNCTIONS_
-#define _VARIADIC_FUNCTIONS_
 
-#include <stdarg.h>
-#include <stdio.h>
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
 
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+
+
 
 #endif
